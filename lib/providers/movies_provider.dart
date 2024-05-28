f44e6410ff49b236a064c96c1e3b1ee044bd3d0d
bitsMaicol.Tascon <maicol_tascon@hotmail.com>
2024-05-28 16:38:03 -0500

    Stream add in suggestons by query and search

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,24 +1,28 @@
-
-
-import 'dart:math';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:movies/helpers/debouncer.dart';
 import 'package:movies/models/models.dart';
 
-
 class MoviesProvider extends ChangeNotifier {
-  final String _apiKey =  'df94e35d6a00ea17f1a05531049a9210';
+  final String _apiKey = 'df94e35d6a00ea17f1a05531049a9210';
   final String _accessToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjk0ZTM1ZDZhMDBlYTE3ZjFhMDU1MzEwNDlhOTIxMCIsInN1YiI6IjYxMDFlMjBlN2Q1ZjRiMDA3NDMxMTFjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9EN0hd44IZ-IwKNAHqgA-nJ6hMFQxvHO8DmPMXNzSH0';
   final String _baseUrl = 'api.themoviedb.org';
   final String _language = 'es-ES';
 
-
   List<Movie> onDisplayMovies = [];
   List<Movie> pupularsMovies = [];
 
   Map<int, List<Cast>> moviesCast = {};
+
+  final debouncer = Debouncer(
+    duration: const Duration(milliseconds: 500),
+  );
+
+  final StreamController<List<Movie>> _suggestionsStrteamController = StreamController.broadcast();
+  Stream<List<Movie>> get suggestionStream => _suggestionsStrteamController.stream;
 
   MoviesProvider() {
     getOnDisplayMovies();
@@ -33,25 +37,22 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getPopulars() async {
-    var response =  await _getJsonData('/3/tv/popular', 1);
+    var response = await _getJsonData('/3/tv/popular', 1);
     final populars = NowPlayingResponse.fromJson(response);
     pupularsMovies = [...pupularsMovies, ...populars.results];
     notifyListeners();
   }
 
-  Future<List<Cast>> getMovieCast( int movieId ) async {
-    if( moviesCast.containsKey(movieId) ) return moviesCast[movieId]!;
+  Future<List<Cast>> getMovieCast(int movieId) async {
+    if (moviesCast.containsKey(movieId)) return moviesCast[movieId]!;
     final jsonData = await _getJsonData('3/movie/$movieId/credits');
-    final creditsResponse = CreditsResponse.fromJson( jsonData );
+    final creditsResponse = CreditsResponse.fromJson(jsonData);
     moviesCast[movieId] = creditsResponse.cast;
     return creditsResponse.cast;
   }
 
-
-
-
-  Future<String> _getJsonData( String endpoint, [int page = 1] ) async {
-    final url = Uri.https( _baseUrl, endpoint, {
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    final url = Uri.https(_baseUrl, endpoint, {
       'Authorization': 'Bearer $_accessToken',
       'api_key': _apiKey,
       'language': _language,
@@ -62,9 +63,8 @@ class MoviesProvider extends ChangeNotifier {
     return response.body;
   }
 
-    Future<List<Movie>> searchMovies( String query) async {
-
-    final url = Uri.https( _baseUrl, '3/search/movie', {
+  Future<List<Movie>> searchMovies(String query) async {
+    final url = Uri.https(_baseUrl, '3/search/movie', {
       'Authorization': 'Bearer $_accessToken',
       'api_key': _apiKey,
       'language': _language,
@@ -72,13 +72,22 @@ class MoviesProvider extends ChangeNotifier {
     });
 
     final response = await http.get(url);
-    final searchResponse = SearchResponse.fromJson(response.body); 
+    final searchResponse = SearchResponse.fromJson(response.body);
     return searchResponse.results;
   }
 
-  getPopularMovies() {}
+  void getSuggestionsByQuery(String query) {
+    debouncer.value = '';
+    debouncer.onValue = (value) async {
+      final results = await searchMovies(value);
+      _suggestionsStrteamController.add(results);
+    };
 
-  
+    final timer = Timer.periodic(const Duration(milliseconds: 300), (_) {
+      debouncer.value = query;
+    });
 
-
+    Future.delayed(const Duration(milliseconds: 301))
+        .then((_) => timer.cancel());
+  }
 }
--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -44,9 +44,9 @@ class MovieSearchDelegate extends SearchDelegate {
     }
 
     final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
-
-    return FutureBuilder(
-      future: moviesProvider.searchMovies(query),
+    moviesProvider.getSuggestionsByQuery( query );
+    return StreamBuilder(
+      stream: moviesProvider.suggestionStream,
       builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
         if (!snapshot.hasData) return _emptyContainer();
 
